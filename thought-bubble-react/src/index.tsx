04d356539054996f1fd3731d82a0ef25d98bd844
Schutz <schutzj@kenyon.edu>
2018-09-19 16:45:40 -0500

    got a static css-working page

--- thought-bubble-react/src/index.tsx
+++ thought-bubble-react/src/index.tsx
@@ -3,6 +3,7 @@ import * as ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+import './stylesheets/entrypoint.css';
 
 ReactDOM.render(
   <App />,
