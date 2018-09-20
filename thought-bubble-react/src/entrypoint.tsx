b692a39cf9d56cba354bae0845a9abbae5f91966
christiangalang <christiangalangpariveda@gmail.com>
2018-09-20 16:59:27 -0500

    added attributes to contain form values

--- thought-bubble-react/src/entrypoint.tsx
+++ thought-bubble-react/src/entrypoint.tsx
@@ -1,13 +1,77 @@
 import * as React from 'react';
 
-class EntryPoint extends React.Component {
-    public onHostRoom() {
-        console.log('click on host room');
+interface IAttributes {
+    hostName: string | undefined,
+    hours: string | undefined,
+    isHost: boolean | undefined,
+    minutes: string | undefined,
+    password: string | undefined,
+    question: string | undefined,
+    userName: string | undefined,
+    userPassword: string | undefined
+}
+
+class EntryPoint extends React.Component<{}, IAttributes> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            hostName: undefined,
+            hours: undefined,
+            isHost: undefined,
+            minutes: undefined,
+            password: undefined,
+            question: undefined,
+            userName: undefined,
+            userPassword: undefined
+        };
     }
 
-    public onJoinRoom() {
-        console.log('clicked on join room');
+    public onHostRoom = () => {
+        this.setState({isHost: true});
+        console.log("isHost = " + this.state.isHost);
     }
+
+    public onJoinRoom = () => {
+        this.setState({isHost: false});
+        console.log("isHost = " + this.state.isHost);
+    }
+
+    public hostSubmission = () => {
+        if (this.state.isHost) {
+            console.log("hostName: " +this.state.hostName);
+
+        }
+    }
+
+    public updateHostValue = (evt: any) => {
+        this.setState({hostName: evt.target.value});
+    }
+
+    public updateQuestionValue = (evt: any) => {
+        this.setState({question: evt.target.value});
+    }
+
+    public updateHoursValue = (evt: any) => {
+        this.setState({hours: evt.target.value});
+    }
+
+    public updateMinutesValue = (evt: any) => {
+        this.setState({minutes: evt.target.value});
+    }
+
+    public updateUserPasswordValue = (evt: any) => {
+        this.setState({userPassword: evt.target.value});
+    }
+
+    public updateUserNameValue = (evt: any) => {
+        this.setState({userName: evt.target.value});
+    }
+
+    public updatePasswordValue = (evt: any) => {
+        this.setState({password: evt.target.value});
+    }
+
+
     public render() {
         return (
             <div>
@@ -33,34 +97,34 @@ class EntryPoint extends React.Component {
                                     <br style={{clear: "both"}}/>
                                     <div className="form-group">
                                         <label>Name:</label>
-                                        <input type="text" className="form-control" id="hostName" name="name"
+                                        <input value={this.state.hostName} onChange={this.updateHostValue} type="text" className="form-control" id="hostName" name="name"
                                                placeholder="Name" required={true}/>
                                     </div>
                                     <div className="form-group">
-                                        <label>Title/ID:</label>
-                                        <input type="text" className="form-control" id="email" name="title"
-                                               placeholder="Title/ID" required={true}/>
-                                    </div>
-                                    <div className="form-group">
                                         <label>Question:</label>
-                                        <input type="text" className="form-control" id="mobile" name="question"
+                                        <input value={this.state.question} onChange={this.updateQuestionValue} type="text" className="form-control" id="question" name="question"
                                                placeholder="Question" required={true}/>
                                     </div>
 
                                     <div className="form-group">
+                                        <label>Password:</label>
+                                        <input value={this.state.password} onChange={this.updatePasswordValue}type="number" name="minutes" min="0" max="59"/>
+                                    </div>
+
+                                    <div className="form-group">
                                         <label>Hours:</label>
-                                        <input type="number" name="hours" min="0" max="24"/>
+                                        <input value={this.state.hours} onChange={this.updateHoursValue} type="number" name="hours" min="0" max="24"/>
                                     </div>
                                     
                                     <div className="form-group">
                                         <label>Minutes:</label>
-                                        <input type="number" name="minutes" min="0" max="59"/>
+                                        <input value={this.state.minutes} onChange={this.updateMinutesValue}type="number" name="minutes" min="0" max="59"/>
                                     </div>
                                 </form>
                             </div>
                             <div className="modal-footer">
                                 <button type="button" className="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                                <button type="button" id="hostStart" className="btn btn-primary">Start Meeting</button>
+                                <button type="button" onClick={this.hostSubmission} id="hostStart" className="btn btn-primary">Start Meeting</button>
                             </div>
                         </div>
                     </div>
@@ -79,18 +143,18 @@ class EntryPoint extends React.Component {
                             <div className="modal-body">
                                 <form>
                                     <div className="form-group">
-                                        <label>Title/ID:</label>
-                                        <input type="text" className="form-control" id="title" name="title"
-                                               placeholder="Title" required={true}/>
+                                        <label>Question:</label>
+                                        <input value={this.state.question} onChange={this.updateQuestionValue} type="text" className="form-control" id="question" name="question"
+                                               placeholder="Question" required={true}/>
                                     </div>
                                     <div className="form-group">
                                         <label>Password:</label>
-                                        <input type="text" className="form-control" id="password" name="password"
+                                        <input value={this.state.userPassword} onChange={this.updateUserPasswordValue} type="text" className="form-control" id="password" name="password"
                                                placeholder="Password" required={true}/>
                                     </div>
                                     <div className="form-group">
                                         <label>Name:</label>
-                                        <input type="text" className="form-control" id="userName" name="name"
+                                        <input value={this.state.userName} onChange={this.updateUserNameValue} type="text" className="form-control" id="userName" name="name"
                                                placeholder="Name" required={true}/>
                                     </div>
                                 </form>
