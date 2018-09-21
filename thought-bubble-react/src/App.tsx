fe2cba59033fcd273a87381f9cb56cbcacd3c82f
Dixon <trevor.dixon@parivedasolutions.com>
2018-09-21 09:29:12 -0500

    Presentation ready

--- thought-bubble-react/src/App.tsx
+++ thought-bubble-react/src/App.tsx
@@ -1,69 +1,32 @@
 import * as React from 'react';
 import {
     HashRouter,
-    Redirect,
+    NavLink,
     Route
 } from 'react-router-dom';
 
 import Suggestion from './suggestion';
-// import EntryPoint from './entrypoint';
+import EntryPoint from './entrypoint';
 import Decision from './decision';
 
-interface IAppState {
-    currentPage: string | null,
-    targetPage: string | null
-}
-
-class App extends React.Component<{}, IAppState> {
+class App extends React.Component<{}> {
     constructor(props: any) {
         super(props);
-        this.state = {
-            currentPage: null,
-            targetPage: null
-        }
-    }
-
-    public loadPage = (pageName: string) => {
-        console.log('load ' + pageName);
-        this.setState({targetPage: pageName});
-    }
-
-    public updateCurrentPage = (page: string) => {
-        this.setState({currentPage: page});
     }
 
     public render() {
-        if(this.state.targetPage === this.state.currentPage) {
-            console.log('r1');
-            // do nothing--we're already at the right page
-        }
-        else if(this.state.targetPage === 'entrypoint') {
-            console.log('loading entrypoint');
-            return <HashRouter><Redirect to='/' /></HashRouter>;
-        }
-        else if(this.state.targetPage === 'suggestion') {
-            console.log('r2');
-            return <HashRouter><Redirect to='/suggestion' /></HashRouter>;
-        }
-        else if(this.state.targetPage === 'decision') {
-            console.log('r3');
-            return <HashRouter><Redirect to='/decision' /></HashRouter>;
-        }
-        else {
-            console.log('targetPage: ' + this.state.targetPage);
-            console.log('currentPage: ' + this.state.currentPage);
-        }
-
         return (
             <HashRouter>
                 <div>
+                    <div>   
+                    <NavLink to="/">Entry Point</NavLink>
+                    <NavLink to="/suggestion">Suggestion</NavLink>
+                    <NavLink to="/decision">Decision</NavLink>
+                    </div>
 
                     <div className="content">
-                        {/*<Route*/}
-                               {/*path="/"*/}
-                               {/*render={(props) => <EntryPoint {...props} onSetTarget={this.loadPage} />}*/}
-                               {/*/>*/}
-                        <Route path="/" component={Suggestion}/>
+                        <Route path="/" component={EntryPoint}/>
+                        <Route path="/suggestion" component={Suggestion}/>
                         <Route path="/decision" component={Decision}/>
                     </div>
                 </div>
